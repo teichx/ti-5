01383c4852d706a2c5e3304cd840e0e1bdad2e44
teichx <teichx@hotmail.com>
2023-11-28 10:57:22 -0300

    refactor: Remove unused code

--- TI5yncronizer/src/TI5yncronizer.Server/Program.cs
+++ TI5yncronizer/src/TI5yncronizer.Server/Program.cs
@@ -1,4 +1,3 @@
-using TI5yncronizer.Core.Authentication;
 using TI5yncronizer.Core.FileWatcher;
 using TI5yncronizer.Server.Context;
 using TI5yncronizer.Server.FileWatcher;
@@ -11,7 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddDbContextFactory<DataContext, DataContextFactory>()
     .AddSingleton<IFileWatcherActions, FileWatcherActions>()
-    .AddSingleton<IAuthSession, AuthSession>()
     .AddSingleton<IFileWatcher, FileWatcher>()
     .AddGrpc();
 
@@ -20,7 +18,6 @@ var app = builder.Build();
 await app.Services.Init();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<AuthService>();
 app.MapGrpcService<FileListenerService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
